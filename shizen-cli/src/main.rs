84159d6beea91cee166064bbcb4b21993d449f41
Brandon Pollack <brandonpollack23@gmail.com>
2024-07-25 01:59:15 +0900

    cli setup

--- shizen-cli/src/main.rs
+++ shizen-cli/src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+  #[command(subcommand)]
+  command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+  ListAllNotes,
+  ListAllActionableNotes,
+  // etc
+}
+
 fn main() {
+  // TODO CLI set up tracing_subscriber.
   println!("Hello, world!");
 }
