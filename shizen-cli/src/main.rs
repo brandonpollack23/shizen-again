8bc633a86bde92fbb0fb18895cbab84ae22962a7
Brandon Pollack <brandonpollack23@gmail.com>
2024-07-26 14:29:01 +0900

    basic setup

--- shizen-cli/src/main.rs
+++ shizen-cli/src/main.rs
@@ -1,20 +1,39 @@
 use clap::{Parser, Subcommand};
+use libshizen::storage::rusqlite::RusqliteStorage;
+use libshizen::storage::TodoStorage;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
+  #[arg(short, long, value_name = "DB FILE", default_value_t = ("~/shizen.db".to_owned()))]
+  database_path: String,
+
   #[command(subcommand)]
-  command: Option<Commands>,
+  command: Commands,
 }
 
 #[derive(Subcommand)]
+#[command(arg_required_else_help(true))]
 enum Commands {
-  ListAllNotes,
-  ListAllActionableNotes,
-  // etc
+  /// List notes/todos.
+  List,
 }
 
 fn main() {
-  // TODO CLI set up tracing_subscriber.
-  println!("Hello, world!");
+  tracing_subscriber::fmt::init();
+
+  let cli = Cli::parse();
+
+  let mut database =
+    RusqliteStorage::new(Some(cli.database_path.into())).expect("Could not open database");
+
+  match cli.command {
+    Commands::List => {
+      // TODO prettier
+      println!(
+        "{:#?}",
+        database.load_all_notes().expect("error loading all notes")
+      );
+    }
+  }
 }
