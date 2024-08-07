f5a4fbf473a1c898f5ae9b49b606ebb5c23ff0af
Brandon Ross Pollack <brandonpollack23@gmail.com>
2024-08-07 14:24:56 +0900

    todos

--- libshizen/src/lib.rs
+++ libshizen/src/lib.rs
@@ -6,3 +6,10 @@ mod sync;
 pub use result::*;
 
 pub type DefaultStorage = storage::rusqlite::RusqliteStorage;
+
+// TODO list
+// * dioxus UI for desktop/tui
+// * Work to run as systemd service server with cli.
+// * Attempt to port to web by making a rusqlite that runs in browser
+//   (either by reimplementing sqlite interface as a trait and delegating to sqlite wasm in Rusqlite, or by using WASI and vfs feature of rusqlite)
+// * merge conflict detection/handling
